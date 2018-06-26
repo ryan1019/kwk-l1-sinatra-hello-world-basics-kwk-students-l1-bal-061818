f094018b18af3ebe3b7a7b3f84d5bdc98b2d5d98
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-26 15:57:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+get '/name' do
+  "My name is Ryan."
 end
+get '/hometown' do
+  "My hometown is Kansas City."
+end
+get '/favorite-song' do
+  "My favorite song is Bad Boy."
+end
+end 
